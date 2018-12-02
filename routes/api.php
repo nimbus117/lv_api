1025cdebb1f8bb81e7a39945916ef5a26bfa73f2
nimbus117 <mail@nimbus117.co.uk>
2018-12-02 15:28:24 +0000

    added ArticleController and updated api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Article;
+
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +18,8 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
